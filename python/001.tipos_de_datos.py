ce1baa48dfd8236b7933905848a138fd897943f7
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-11 11:40:07 +0100

    pruebas

---
+++ python/001.tipos_de_datos.py
@@ -0,0 +1,16 @@
+# Esto es un comentario 
+
+# Strings textos, formas equivalentes de crear texto:
+# En los nombres de las variables evitar: acentons y ñ y empezar por número
+texto1 = " Hola que tal"
+texto2 = 'Esto es otro texto'
+
+texto3 = """Hola esto es otro texto 
+        Esto es otra línea del mismo texto
+        Otra línea más
+        """
+
+# Equivalente a Escribir de PSeInt
+print(texto1)
+print(texto2)
+print(texto3)
