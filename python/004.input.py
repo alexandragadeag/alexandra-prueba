379f2734b97d0bffb5fd1491413192a7bd1a5ed3
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-11 14:25:11 +0100

    input

---
+++ python/004.input.py
@@ -0,0 +1,25 @@
+
+# input lee todos los datos como string
+
+nombre = input('Por favor intoduce tu nombre: ')
+print("El nombre es", nombre)
+
+edad = input('Introduce edad: ')
+print("Edad es", edad)
+edad_num = int(edad) # convierte de texto a número entero int
+print(edad_num + 1) # probamos una operación aritmética cualquiera
+
+# leer peso con decimales float()
+peso = input('Introduce tu peso: ')
+peso_num = float(peso)
+print(peso_num - 5)
+
+
+# leer si está en alta o no bool()
+
+
+
+
+
+
+
