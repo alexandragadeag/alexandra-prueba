72ede0011f0796d5da0c4d50758e6e9e0fb73382
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-12 11:00:10 +0100

    python operadores aritmeticos

---
+++ python/005.type.py
@@ -0,0 +1,12 @@
+
+
+# La función type() permite saber si un dato es de un tipo concreto
+
+print(type("Hola")) # str
+print(type('Hola')) # str
+
+print(type(50)) # int
+print(type(2000.44)) # float
+
+print(type(True)) # bool
+print(type(False)) # bool 
---
+++ python/006.operadores_aritmeticos.py
@@ -0,0 +1,8 @@
+
+print(50 / 34) # División exacta 1.4705882352941178
+print(round(50 / 35, 2)) # Redondear el resultado a 2 decimales
+
+print(50 // 34) # División entera 1, elimina la parte decimal 
+
+print(2 ** 3) # Exponente
+
