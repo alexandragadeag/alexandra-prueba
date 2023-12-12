9b4dadc4057b3b8b98db3fc1c45f21cf7d3365e1
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-12 11:44:25 +0100

    python operadores aritmeticos

--- python/006.operadores_aritmeticos.py
+++ python/006.operadores_aritmeticos.py
@@ -6,3 +6,14 @@ print(50 // 34) # División entera 1, elimina la parte decimal
 
 print(2 ** 3) # Exponente
 
+# Ejemplo de uso división  entera e un contexto en el que los decimales no tiene
+total_regalos = 123
+total_mesa = 27
+
+regalos_por_mesa = total_regalos // total_mesas
+
+# f string: string formatedado, es un string que permite incrustar una variable detnro
+print(f"Cada mesa recibirá {regalos_por_mesa} regalos.")
+
+print(2 ** 3) # Exponente 
+
