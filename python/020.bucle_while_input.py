45d89b4d9e07dc147f029a2e0be5e6a5109829b2
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-13 14:14:01 +0100

    python while con input

--- python/020.bucle_while_input.py
+++ python/020.bucle_while_input.py
@@ -6,3 +6,12 @@ while password != password_correct:
     password = input("Introduce la contraseña: ").replace(" ", " ")
 
 print("Contraseña correcta. ¡Bienvenido!")
+
+departamentos = ['marketing', 'desarrollo', 'soporte', 'IA']
+departamento = ''
+
+while departamento not in departamentos:
+    departamento = input('Introduce el departamento: ')
+
+print('Departamento correcto.')
+
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+while departamento not in departamentos:
+    departamento = input('Introduce el departamento')
+
+print('Departamento correcto.')
