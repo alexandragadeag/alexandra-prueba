e02b06abafa186cd8efdc37f2922f6b73f8e3eee
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-13 12:37:54 +0100

    python if else elif con punto de interrupción

---
+++ python/016.if_else.py
@@ -0,0 +1,36 @@
+
+
+precio = float(input('Intoduce un precio: '))
+
+if precio > 100: # > 100 es caro
+    print('Es caro')
+elif precio >= 50: # >= 50 es medio
+    print('Es medio')
+else: # es barato
+    print('Es barato')
+
+print('fin')
+
+
+# if
+
+# if else
+
+# if elif else
+
+
+# RUTINA DIARA
+hora = 12
+
+if 0 <= hora <= 8:
+    print('Durmiendo...zzz...💤😴😴')
+elif 8 < hora <- 15:
+    print('Curso Angular'💻📚)
+elif 15 < hora <- 18:
+    print('Comiendo paella🥘')
+elif 18 < hora <-21:
+    print('En el gimnasio💪🤸')
+elif 21 < hora <= 24:
+    print('Cenando🥣')
+else:
+    print('La hora no es correcta')
