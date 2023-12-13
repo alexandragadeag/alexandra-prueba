bc52b8388dedad759bdc1b37678399f2bf62b9e1
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-13 10:47:00 +0100

    python listas agregar elementos

---
+++ python/012.listas_append.py
@@ -0,0 +1,10 @@
+
+
+clothes = ['Camiseta', 'Pantalón', 'Abrigo', 'Zapatos']
+print(clothes)
+
+clothes.append('Bufanda') # Agrega un elemento al final
+print(clothes)
+
+clothes.insert(2, 'Reloj') # Inserta el reloj antes del abrigo
+print(clothes)
