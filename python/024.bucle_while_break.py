a77622a7a1e80d8473aee9b1bd2f69cfff863fb2
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-15 12:48:50 +0100

    python bucle while break

---
+++ python/024.bucle_while_break.py
@@ -0,0 +1,16 @@
+
+
+# Bucle while con break
+# Se utiliza para ejecutar un bloque de código repetidamente
+# siempre que se cumpla una condición dada.
+
+# Bucle que imprima ciertos números
+
+contador = 0 # inicializando el contador
+while contador <10:
+    print(contador)
+    contador += 1
+    if contador == 5:
+        break
+    
+
