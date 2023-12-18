55b9e82486db09d1fe31f3a1b50c26f1065622eb
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-18 11:01:32 +0100

    python funciones sumatorio

--- python/028.funciones.py
+++ python/028.funciones.py
@@ -11,6 +11,7 @@ def sumatorio(numeros):
     Args:
         numeros (list): lista de números int float
     """
+    
     sumatorio = 0
     
     for num in numeros:
@@ -20,4 +21,10 @@ def sumatorio(numeros):
 
 numeros = [1, 2, 3, 4, 5]
 resultado = sumatorio(numeros)
+resultado2 = sumatorio([100,200,300,400]) #Lo puedes reutilizar y llamar cuantas veces quieras 
 print(resultado)
+print(resultado2)
+
+
+def contar_vocales(Frase):
+    pass
