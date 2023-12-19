570d9feb224a198d7dc53e75537a6efeceaa3b62
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-19 11:01:06 +0100

    python ejercicio aplicación

---
+++ python/py.ejercicio/app.py
@@ -0,0 +1,23 @@
+''''
+Apliación python de consola para gestión de contactos utilizando
+una lista en Python y operciones CRUD (base de cualquier aplicación hoy en día):
+
+Create 
+Retrieve/Read/Leer/Ver/consultar
+Update
+Delete
+
+
+Crear una lista de contactos con al menos 3 contactos (nombres).
+
+Bucle while infinito que imprima un menú de opciones:
+1 - Ver todos los contactos
+2 - Ver un contacto # nombre y ya
+3 - Insertar nuevo contacto
+4 - Acualizar contacto exitente
+5 - Borrar contacto 
+6 - Salir (break)
+
+Utilizar los métodos de input_utils para leer de consola.
+'''
+
