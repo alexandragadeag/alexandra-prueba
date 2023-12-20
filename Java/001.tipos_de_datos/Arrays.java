599d2ac65f54375b08d82119c0bda47a877b6450
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-20 14:23:30 +0100

    Java Arrays

---
+++ Java/001.tipos_de_datos/Arrays.java
@@ -0,0 +1,32 @@
+public class Arrays {
+    
+    public static void main(String[] args) {
+
+        // Array es una estructura de datos
+        
+        int[] numeros = new int[5];
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+        // numeros[5] = 60; // Da error
+        // Si intentamos poner un tipo de dato donde no es te da un error y no te deja compilar.
+
+        // IMPRIMIR LA LONGITUD DEL ARRAY, 5
+        System.out.println(numeros.length);
+        System.out.println(numeros[2]); // 30
+
+        String[] nombres = new String[3]; // Longitud 3
+        nombres[0] = "Alexandra";
+        nombres[1] = "Marian";
+
+        System.out.println(nombres[0]);
+
+        
+        
+
+
+    }
+
+}
