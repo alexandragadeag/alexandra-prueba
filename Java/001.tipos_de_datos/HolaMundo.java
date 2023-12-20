ae38e50bb6792b908755593cdfe1b8eb4609e0f2
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-20 12:03:52 +0100

    Java Hola Mundo y prueba

---
+++ Java/001.tipos_de_datos/HolaMundo.java
@@ -0,0 +1,14 @@
+
+// Comentario de una sola línea
+ /*
+  Comentario de varias líneas
+  en Java
+  */
+
+public class HolaMundo {
+
+  public static void main(String[] args){
+    // con sout + tabulador se escribe System.out.println().
+    System.out.println("Hola Mundo");
+  }
+}
---
+++ Java/Prueba.java
@@ -0,0 +1,15 @@
+
+public class Prueba {
+// Si ponemos la palabra main, se crea el método main. Solo puede haber un método main en cada archivo 
+    public static void main(String[] args) {
+        // Esto es un comentario
+        System.out.println("Hola soy Alexandra, prueva desde Java");
+        /*
+         * Esto es un comentario
+         * que contiene varias líneas
+         * Ejemplo de comentario
+         */
+        System.out.println("Hola soy Alexandra, prueva desde Java");
+        System.out.println("Hola soy Alexandra, prueva desde Java");
+    }   
+}
