89b570bb5f5099a4fdbd77f26bce8724485d4ec0
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-21 11:57:07 +0100

    Operador logico Not Java

---
+++ Java/002.operadores/OperadorLogicoNot.java
@@ -0,0 +1,14 @@
+public class OperadorLogicoNot {
+
+    public static void main(String[] args) {
+
+        // Negación, not, en Java !
+
+        boolean isAdult = false;
+        boolean isNotAdult = !isAdult; // se invierte el valor boolean
+        System.out.println(isAdult);
+        System.out.println(isNotAdult);
+
+    }
+    
+}
