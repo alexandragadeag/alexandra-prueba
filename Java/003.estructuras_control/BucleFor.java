e11d4a83c1ee93b60e191bbd20c64cd17320dfd7
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-21 13:50:11 +0100

    BucleFor Java

---
+++ Java/003.estructuras_control/BucleFor.java
@@ -0,0 +1,22 @@
+public class BucleFor {
+
+    public static void main(String[] args) {
+
+        // Operador incremento ++
+        // Operador decremento --
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            
+         }
+          // for (start; stop; step)
+          for (int i = 0; i < 20; i += 2) {
+            System.out.println(i);
+
+          } 
+        }
+        
+
+    }
+    
+
