dabffbecf7bd502a5e35e7d8f849ce32e00db19d
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2023-12-21 11:52:56 +0100

    Java operador logico Or

---
+++ Java/002.operadores/OperadorLogicoOr.java
@@ -0,0 +1,34 @@
+
+
+/**
+ * and equivale a && en java
+ * or equivale a || en java
+ * not equivale a ! en java 
+ */
+public class OperadorLogicoOr {
+
+    public static void main(String[] args) {
+
+        // disyunción, or, ||. Con que no sea true ya el resultado es true
+        boolean isStudent = false;
+        int age = 80;
+
+        boolean isEligible = isStudent || age < 60;
+        System.out.println(isEligible);
+
+        // Combinar conjunción y disyunción
+
+
+        isStudent = true;
+        age= 15;
+        // age > 20 y age < 80 o isStudent
+        isEligible = isStudent || age > 20 && age < 80;
+        System.out.println(isEligible);
+        
+
+
+    }
+    
+
+
+}
--- Java/002.operadores/OperadoresLogicoAnd.java
+++ Java/002.operadores/OperadoresLogicoAnd.java
@@ -1,4 +1,4 @@
-public class OperadoresLogicos {
+public class OperadoresLogicoAnd {
 
     public static void main(String[] args) {
 
