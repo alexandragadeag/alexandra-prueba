e61928278c80c2581e33b0810cc849609f329595
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-08 13:06:07 +0100

    Bucle While Java

---
+++ Java/003.estructuras_control/BucleWhile.java
@@ -0,0 +1,45 @@
+public class BucleWhile {
+
+    public static void main(String[] args) {
+        
+       int contador = 0;
+       while (contador < 10) {
+           contador++;
+           System.out.println("contador: " + contador);
+
+       }
+       // Ulitilizar bucle para encontrar a Wally, detener el bucle al encontrarlo 
+       String[] nombres = {"Juan", "Alexandra", "Wally", "Bob"};
+
+       // boolean found
+       // while break
+       // The local variable encontrado may not have been initialized
+       boolean encontrado = false; // false por defecto
+       int count = 0;
+
+       while (count < nombres.length) {
+           boolean resultado = nombres[count] == "Wally";
+
+           System.out.println(nombres[count] + " igual a Wally es " + resultado);
+
+           if (resultado) {
+              break;
+
+           } else {
+            System.out.println("No se ha encontrado a Wally sigo buscando");
+           }
+
+           count++; // incrementa en 1
+           
+       }
+
+       // count 0 < 4
+       // count 1 < 4
+       // count 2 < 4
+       // count 3 < 4
+       // count 4 < 4
+       // count 4 < 4 se inclumple
+
+    }
+    
+}
