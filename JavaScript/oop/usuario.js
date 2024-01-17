a88d3b2ecf5bdbabe28f52cd0020e1e2bfb0fd86
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-17 10:14:19 +0100

    Oop usuario javascript

---
+++ JavaScript/oop/usuario.js
@@ -0,0 +1,26 @@
+
+// Creación de una clase
+class Usuario {
+
+    constructor(firstName, email, phone, age ){
+         this.firstName = firstName;
+         this.email = email;
+         this.phone = phone;
+         this.age = age;
+
+    }
+    incrementarEdad() {
+        // Incrementar age en 1 porque ha cumplido años
+        this.age++;
+    }
+}
+
+// Crear 3 objetos Usuario
+let usuario1 = new Usuario("Usuario1", "user1mail.com", "11111111", 20);
+console.log(usuario1);
+
+usuario1.incrementarEdad()
+console.log(usuario1);
+
+let usuario2 = new Usuario("usuario2", "user2@gmail.com", "222222222", 31);
+console.log(usuario2);
