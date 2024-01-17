3e6d5835a7ff282f2a4a24f72d4810eaa2c70adc
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-17 13:47:02 +0100

    Create author_book.js

---
+++ JavaScript/composición/author_book.js
@@ -0,0 +1,24 @@
+
+//Crear la clase Author
+// nombre, edad, bio, numBestSeller, estudios[]
+class Author{
+
+}
+
+// Crear la clase editorial
+// nombre, year, numPublishedBooks
+class Editorial{
+
+}
+
+//Crear la clase libro
+// isbn,title, numPages, price, author, editorial
+class Book{
+
+    
+}
+
+
+// Crear obejtos autor y editorial
+
+// Crear objetos libro 
