f9e0ce0a23dca98b754e2b306acfebbab44a839c
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-24 13:11:46 +0100

    contador incrementar, decrementar, resetear

--- Angular/Proyecto/src/app/contador/contador.component.ts
+++ Angular/Proyecto/src/app/contador/contador.component.ts
@@ -8,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './contador.component.css'
 })
 export class ContadorComponent {
+// Crear variable contador number sin valor
+contador: number | undefined;
+// método para incrementar
+incrementar() {
+  if(!this.contador) {
+    this.contador = 0;
+  }
+  this.contador++;
+}
+// método para decrementar
+decrementar() {
+  if(!this.contador)
+    this.contador = 0;
+
+  if(this.contador > 0) // condicion para evitar decrementar por debajo de 0
+    this.contador--;
+}
+
+// método para resetear a undefined
+resetear() {
+  const reset = confirm("Esta operación elimina el contador, ¿Quiere proceder?");
+  if (reset)
+    this.contador = undefined;
+}
+
+// método getCounterStyle para obtener color en base a número
+// y usarlo con la directiva ngStyle
+
+// deshabilitar
 
 }
