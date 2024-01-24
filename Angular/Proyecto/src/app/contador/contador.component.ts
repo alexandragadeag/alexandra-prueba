c634a0797ff0c979c9e2019cca32d6ea6fd9984b
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-24 14:13:07 +0100

    contador colores Angular

--- Angular/Proyecto/src/app/contador/contador.component.ts
+++ Angular/Proyecto/src/app/contador/contador.component.ts
@@ -1,9 +1,10 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contador',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './contador.component.html',
   styleUrl: './contador.component.css'
 })
@@ -35,7 +36,16 @@ resetear() {
 
 // método getCounterStyle para obtener color en base a número
 // y usarlo con la directiva ngStyle
-
-// deshabilitar
-
+getCounterStyle(): any {
+  if (this.contador && this.contador < 5) {
+  return {color: "red"};
+  
+  } else if (this.contador && this.contador < 10) {
+  return {color: "blue"};
+  
+  } else {
+  return {color: "green"};
+  }
+  
+  }
 }
