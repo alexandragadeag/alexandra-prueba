5faa9f92ca6e9921c50a69e551bdc71cce38afc9
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-24 12:11:12 +0100

    crear componente Angular

--- Angular/Proyecto/src/app/app.component.ts
+++ Angular/Proyecto/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ContadorComponent } from "./contador/contador.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ProductListComponent, ProductDetailComponent]
+    imports: [RouterOutlet, ProductListComponent, ProductDetailComponent, ContadorComponent]
 })
 export class AppComponent {
   title = 'Angular';
