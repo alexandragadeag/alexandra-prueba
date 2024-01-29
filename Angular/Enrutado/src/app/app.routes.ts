b43a7b992bd1e9c863c69182136fdde3f8a4b747
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-29 14:11:31 +0100

    RouterLink

--- Angular/Enrutado/src/app/app.routes.ts
+++ Angular/Enrutado/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantFormComponent } from './restaurant-form/restaurant-form.component';
 
 export const routes: Routes = [
+    {
+        path: 'restaurantes',
+        component: RestaurantListComponent
+    },
+    {
+        path: 'restaurantes/formulario',
+        component: RestaurantFormComponent
+    }
     // definir las rutas dela aplicación
 
     // restaurantes --> RestaurantList
@@ -8,6 +18,6 @@ export const routes: Routes = [
     //restaurantes/details --> RestauranteDetail
 
 
-    
+
 ];
 
