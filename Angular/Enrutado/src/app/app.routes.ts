324ed5765a6b3a31fc24aaff38636b953817c606
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-29 13:19:31 +0100

    enrutado borrar HTML

--- Angular/Enrutado/src/app/app.routes.ts
+++ Angular/Enrutado/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // definir las rutas dela aplicación
+
+    // restaurantes --> RestaurantList
+
+    //restaurantes/details --> RestauranteDetail
+
+
+    
+];
+
