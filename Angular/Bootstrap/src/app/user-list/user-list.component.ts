5b50367e4be7f11589650c76be67dedf21377bf9
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-29 10:46:50 +0100

    Angular tabla agregar botones

--- Angular/Bootstrap/src/app/user-list/user-list.component.ts
+++ Angular/Bootstrap/src/app/user-list/user-list.component.ts
@@ -25,7 +25,7 @@ export class UserListComponent implements OnInit{
     this.users = [
       {
         id: 1,
-        email: "user1gmail.com",
+        email: "user1@gmail.com",
         salary: 2000.0,
         active: true,
         registerDate: new Date()
@@ -33,11 +33,32 @@ export class UserListComponent implements OnInit{
       },
       {
         id:2,
-        email: "user2gmail.com",
+        email: "user2@gmail.com",
         salary: 3000.0,
         active: false,
         registerDate: new Date()
-      }
+      },
+      {
+        id:3,
+        email: "user3@gmail.com",
+        salary: 2000.0,
+        active: true,
+        registerDate: new Date()
+      },
+      {
+        id:4,
+        email: "user4@gmail.com",
+        salary: 5000.0,
+        active: false,
+        registerDate: new Date()
+      },
+      {
+        id:5,
+        email: "user5@gmail.com",
+        salary: 3000.0,
+        active: true,
+        registerDate: new Date()
+      },
     ]
   }
 
