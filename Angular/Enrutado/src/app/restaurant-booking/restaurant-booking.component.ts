d9ad9724a3d65469985d8c4214c08bb8e0eb68b8
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-01-31 09:50:59 +0100

    Angular componente restaurant-booking

---
+++ Angular/Enrutado/src/app/restaurant-booking/restaurant-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-booking',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurant-booking.component.html',
+  styleUrl: './restaurant-booking.component.css'
+})
+export class RestaurantBookingComponent {
+
+}
