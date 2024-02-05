7d43853966bb900063140d25f19b037778200b1f
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-05 12:29:34 +0100

    Angular Inyectar HTTPCLIENT en servicio

--- Angular/Servicios/src/app/product-list/product-list.component.ts
+++ Angular/Servicios/src/app/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
+  providers: [ProductService],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
--- Angular/Servicios/src/app/services/product.service.ts
+++ Angular/Servicios/src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,10 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
+  constructor(private http: HttpClient) { }
+
   holaMundo(): string{
     return "Hola mundo";
 
-  }
-
-  constructor() { }
+  } 
 }
