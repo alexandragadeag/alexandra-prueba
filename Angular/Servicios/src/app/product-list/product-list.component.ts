4c256afe1dc36769a033b432baa999e9150683f6
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-05 14:11:17 +0100

    Angular lista de productos

--- Angular/Servicios/src/app/product-list/product-list.component.ts
+++ Angular/Servicios/src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Product } from '../Interfaces/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -10,9 +11,16 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  products: Product[] = []; // ARRAY DE PRODUCTOS QUE VIENEN DE FAKESTORE API
 
   constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    // llamar al servicio y traer los productos de FAKE STORE API
+    this.productService.findall().subscribe(data => this.products = data);
+  }
 
   imprimirHola(): void {
     let texto = this.productService.holaMundo();
