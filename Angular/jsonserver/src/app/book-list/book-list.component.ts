31d35862dd133189610abfb3d5b6eb8f85f74929
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-07 12:08:24 +0100

    Angular componente con httpClient SIN SERVICIO

--- Angular/jsonserver/src/app/book-list/book-list.component.ts
+++ Angular/jsonserver/src/app/book-list/book-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../interfaces/book.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [ HttpClientModule ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
 
+  // array de books
+
+  books: Book[] = [];
+
+  // constructor con httpClient
+  constructor(private http: HttpClient) {}
+ 
+
+  // ngOnInit inicialiazar el array trayendo libros con httpClient
+  // Ejemplo SIN SERVICIO
+  ngOnInit(): void {
+   this.http.get<Book[]>('http://localhost:3000/books').subscribe(books => this.books = books);
+  }
+  
 }
