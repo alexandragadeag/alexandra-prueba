b36b1de5eb3cd720f90cae3b3539937014551f67
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-08 11:15:35 +0100

    Cambios Author-list component

--- Angular/jsonserver/src/app/author-list/author-list.component.ts
+++ Angular/jsonserver/src/app/author-list/author-list.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Author } from '../interfaces/author.model';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
+import { AuthorService } from '../services/author.service';
 
 @Component({
   selector: 'app-author-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
+  providers: [AuthorService],
   templateUrl: './author-list.component.html',
   styleUrl: './author-list.component.css'
 })
-export class AuthorListComponent {
+export class AuthorListComponent implements OnInit{
+
+  authors: Author[] = [];
+  constructor(private authorService: AuthorService) {}
+
+  ngOnInit(): void {
+   this.loadAuthors();
+  }
+  loadAuthors(){
+    this.authorService.findAll()
+    .subscribe(authors => this.authors = authors);
+  }
+  deleteAuthor(id: string | number) {
+    const remove: boolean = confirm("¿Quieres borrar e autor de verdad?");
+
+    if(!remove) return;
+
+    this.authorService.deleteById(id).subscribe(() => {
+      this.loadAuthors(); //refresca la tabla para que desaparezca el author borrado 
+    });
+
+
+  }
+
+  
 
 }
