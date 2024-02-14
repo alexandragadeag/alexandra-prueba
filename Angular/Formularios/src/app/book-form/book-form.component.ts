3f6caca24d43a763341362fe85e44785b7a1fffc
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-14 10:19:33 +0100

    formulario

--- Angular/Formularios/src/app/book-form/book-form.component.ts
+++ Angular/Formularios/src/app/book-form/book-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './book-form.component.html',
   styleUrl: './book-form.component.css'
 })
 export class BookFormComponent {
+
+  bookForm = new FormGroup ({
+    title: new FormControl('')
+
+  });
+
+  save(): void{
+    const title = this.bookForm.get('title')?.value;
+  }
 
 }
