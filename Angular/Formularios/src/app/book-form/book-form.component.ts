079294eda45fe5e768549cdf914000e95a1d2678
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-14 10:59:40 +0100

    Angular formulario libros campos number

--- Angular/Formularios/src/app/book-form/book-form.component.ts
+++ Angular/Formularios/src/app/book-form/book-form.component.ts
@@ -11,12 +11,23 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class BookFormComponent {
 
   bookForm = new FormGroup ({
-    title: new FormControl('')
-
+    title: new FormControl(''),
+    numPages: new FormControl(150),
+    price: new FormControl()
   });
 
   save(): void{
     const title = this.bookForm.get('title')?.value;
+    console.log(title);
+
+    const numPages =
+    this.bookForm.get('numPages')?.value;
+    console.log(numPages);
+
+    const price =
+    this.bookForm.get('price')?.value;
+    console.log(numPages);
+    
   }
 
 }
