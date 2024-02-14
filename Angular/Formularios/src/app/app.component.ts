c6eebe9ac383b8047f6976c7d9ea8f0606505bb5
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-14 10:39:32 +0100

    book-form

--- Angular/Formularios/src/app/app.component.ts
+++ Angular/Formularios/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookFormComponent } from "./book-form/book-form.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, BookFormComponent]
 })
 export class AppComponent {
   title = 'Formularios';
