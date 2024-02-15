9d6c0771f9efbc1d6fc7f4ca83e141e42a46b227
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-15 10:43:58 +0100

    Product-form.component.ts

--- Angular/Bootstrap-form/src/app/product-form/product-form.component.ts
+++ Angular/Bootstrap-form/src/app/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Manufacturer } from '../interfaces/manufacturer.model';
+import { Category } from '../interfaces/category.model';
 @Component({
   selector: 'app-product-form',
   standalone: true,
@@ -9,5 +10,46 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './product-form.component.css'
 })
 export class ProductFormComponent {
+  //Estos datos se recuperarían del BACKEND
+  //utilizando un SERVICIO o
+  //directamente un httpClient y peticiones GET
+
+    manufacturers: Manufacturer[] = [{
+      id: 1,
+      name: "FilaMotor",
+      year: 2012
+      }, {
+      id: 2,
+      name: "Pepsi",
+      year: 1920
+      },
+      {
+      id: 3,
+      name: "Coca Cola",
+      year: 1915
+      }
+      ];
+      categories: Category[] = [
+        { id: 1, name: "Bebidas"},
+        { id: 2, name: "Carne"},
+        { id: 3, name: "Legumbres"},
+        { id: 4, name: "Vegan"}];
+
+        ProductForm = new FormGroup({
+          id: new FormControl({value:'',disabled: true}),
+          title: new FormControl(0,[Validators.required, Validators.min(0), Validators.max(500)]),
+          available: new FormControl(false),
+          publishDate: new FormControl(null),
+          manufacturer: new FormControl(null),
+          categories: new FormControl(null),
+
+        });
+
+        save(): void {
+          
+        }
+
+
+  
 
 }
