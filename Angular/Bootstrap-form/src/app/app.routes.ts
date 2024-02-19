def7205c5b6ecb4275e2ee8aa9c7c10191ca0584
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-19 10:13:55 +0100

    creación de componentes y path

--- Angular/Bootstrap-form/src/app/app.routes.ts
+++ Angular/Bootstrap-form/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path:'',
+    component: ProductListComponent
+   },
+   {
+   path:'products/:id/detail',
+   component: ProductDetailComponent
+   },
+   {
+    path: 'products/create',
+    component: ProductFormComponent
+   },
+   {
+    path: 'products/:id/update',
+    component: ProductFormComponent
+   }
+
+];
