3a77b296d432345a775b0f5646e4085c17359031
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-19 11:00:13 +0100

    Angular product-detail html y ts

--- Angular/Bootstrap-form/src/app/product-detail/product-detail.component.ts
+++ Angular/Bootstrap-form/src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../interfaces/product.model';
+import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, DatePipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
+  
+  product: Product | undefined;
+  constructor(private http: HttpClient,
+    private activatedRoute: ActivatedRoute) { }
+    
+    ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+    let id = params['id'];
+    this.http.get<Product>(`http://localhost:3000/products/${id}`)
+    .subscribe(product => this.product = product);
+    });
+    }
 
 }
--- Angular/Bootstrap-form/src/app/product-list/product-list.component.ts
+++ Angular/Bootstrap-form/src/app/product-list/product-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '../interfaces/product.model';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink, DatePipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) { }
+
+
+ngOnInit(): void {
+this.http.get<Product[]>('http://localhost:3000/products')
+.subscribe(products => this.products = products); }
+
+    
+
 
 }
