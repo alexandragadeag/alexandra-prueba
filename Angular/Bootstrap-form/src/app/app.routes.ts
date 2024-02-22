83ce092f675c906c6620dfd929face6cb81234e9
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-22 19:12:26 +0100

    Angular correcciones para el buen funcionamiento

--- Angular/Bootstrap-form/src/app/app.routes.ts
+++ Angular/Bootstrap-form/src/app/app.routes.ts
@@ -3,37 +3,40 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ManufacturerListComponent } from './manufacturer-list/manufacturer-list.component';
+import { ManufacturerDetailComponent } from './manufacturer-detail/manufacturer-detail.component';
 
 export const routes: Routes = [
-   {
-      path: '',
-      redirectTo: '/product-list',
-      pathMatch: 'full'
-
-   },
-   
-   {
-    path:'product-list',
+  // Listado de productos
+  {
+    path: '',
+    redirectTo: '/product-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'product-list',
     component: ProductListComponent
-   },
-   {
-   path:'products/:id/detail',
-   component: ProductDetailComponent
-   },
-   {
+  },
+  // La pantalla de detalle de un solo producto
+  {
+    path: 'products/:id/detail',
+    component: ProductDetailComponent
+  },
+  // La pantalla de creación de un nuevo producto
+  {
     path: 'products/create',
     component: ProductFormComponent
-   },
-   {
+  },
+  // La pantalla de actualizar un prod existente
+  {
     path: 'products/:id/update',
     component: ProductFormComponent
-   },
-   {
-      path: 'manufacturers',
-      component: ManufacturerListComponent
-   },
-   {
-      path: 'manufacturers/:id/detail',
-      component: ManufacturerListComponent
-   }
+  },
+  {
+    path: 'manufacturers',
+    component: ManufacturerListComponent
+  },
+  { // http://localhost:4200/manufacturers/20/detail
+    path: 'manufacturers/:id/detail',
+    component: ManufacturerDetailComponent
+  }
 ];
--- Angular/Bootstrap-form/src/app/manufacturer-detail/manufacturer-detail.component.ts
+++ Angular/Bootstrap-form/src/app/manufacturer-detail/manufacturer-detail.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { Manufacturer } from '../interfaces/manufacturer.model';
 
 @Component({
   selector: 'app-manufacturer-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink, NgbCarouselModule],
   templateUrl: './manufacturer-detail.component.html',
   styleUrl: './manufacturer-detail.component.css'
 })
-export class ManufacturerDetailComponent {
+export class ManufacturerDetailComponent implements OnInit{
+
+  manufacturer: Manufacturer | undefined;
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+
+  constructor(private http: HttpClient,
+    private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.http.get<Manufacturer>(`http://localhost:3000/manufacturers/${id}`)
+        .subscribe(manufacturer => this.manufacturer = manufacturer);
+    });
+  }
+
 
 }
