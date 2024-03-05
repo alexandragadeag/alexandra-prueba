87a25ed9fe374f6cc4136a97e220bc27e1f03d91
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-05 13:40:37 +0100

    Nest crear controler

--- NestJs/nest-001-holamundo/src/app.module.ts
+++ NestJs/nest-001-holamundo/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookController } from './book/book.controller';
+import { AuthorController } from './author/author.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, BookController],
+  controllers: [AppController, BookController, AuthorController],
   providers: [AppService],
 })
 export class AppModule {}
