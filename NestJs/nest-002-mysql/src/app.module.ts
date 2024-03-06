866948e6a3dae9135714d773cff9c7d40b7aaf7f
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-06 12:26:53 +0100

    Nest Install configuración de MySQL

---
+++ NestJs/nest-002-mysql/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'maricarmen27',
+      database: 'nest', // crear esta base de datos en MYSQL primero 
+      entities: [],
+      synchronize: true, // generar tablas en base de datos 
+      logging: true
+    })
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
