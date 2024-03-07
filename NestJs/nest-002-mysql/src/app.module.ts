c842a4b712230894d4ccf95e44071bbcd75a8001
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-07 13:52:11 +0100

    Nest entidad author y configuración

--- NestJs/nest-002-mysql/src/app.module.ts
+++ NestJs/nest-002-mysql/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './book/book.model';
 import { BookController } from './book/book.controller';
+import { Author } from './author/author.model';
+import { AuthorController } from './author/author.controller';
 
 @Module({
   imports: [
@@ -14,14 +16,14 @@ import { BookController } from './book/book.controller';
       username: 'root',
       password: 'admin1234',
       database: 'nest', // crear esta base de datos en MYSQL primero 
-      entities: [Book],
+      entities: [Book, Author],
       synchronize: true, // generar tablas en base de datos 
       logging: true
     }),
     TypeOrmModule.forFeature([Book]) // Esto permite acceder a Repository
 
   ],
-  controllers: [AppController, BookController],
+  controllers: [AppController, BookController, AuthorController],
   providers: [AppService],
 })
 export class AppModule {}
