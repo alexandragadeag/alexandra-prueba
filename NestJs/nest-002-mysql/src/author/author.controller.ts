2c7c2514e59a2afd4ca3e63ce19209c77f12290d
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-12 10:43:14 +0100

    Nest filtros sobre entidad author

--- NestJs/nest-002-mysql/src/author/author.controller.ts
+++ NestJs/nest-002-mysql/src/author/author.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseFloatPipe, ParseIntPipe } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Author } from './author.model';
-import { Repository } from 'typeorm';
+import { MoreThanOrEqual, Repository } from 'typeorm';
 
 @Controller('author')
 export class AuthorController {
@@ -15,5 +15,51 @@ export class AuthorController {
     findAll() {
         return this.aurthorRepo.find();
     }
+    @Get(':id')
+    findById(@Param('id', ParseIntPipe) id: number) {
+       return this.aurthorRepo.findOne({
+         where:{
+            id: id
+         }
+
+       });
+    }
     
+    //http://localhost:3000/author/filter-full-name/alexandra/García
+    @Get('filter-full-name/:firstname/:lastname')
+    findByFirstNameAndLastName(
+        @Param('firstname') firstname: string,
+        @Param('lastname') lastname: string,
+        ) {
+        
+        // filtrar por nombre y apellido    
+        return this.aurthorRepo.find({
+            where: {
+            
+                 firstName: firstname,
+                 lastName: lastname
+
+            }
+        });
+
+    }
+
+    @Get('filter-by-salary/:salary')
+    findBySalary(@Param('salary', ParseFloatPipe) salary: number) {
+
+        return this.aurthorRepo.find({
+            where:{
+                // coincidencia exacta
+                // salary: salary
+
+                // mayor o igual:
+                salary: MoreThanOrEqual(salary)
+            },
+            order: {
+                salary: "DESC"
+            }
+        });
+      
+    }
+     
 }
