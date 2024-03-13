9874395fc0bd33a8e8e9b7ee57fa30b78ebcd286
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-13 14:10:23 +0100

    Nest instalar libreria Open Api para testear controladores

--- NestJs/nest-002-mysql/src/main.ts
+++ NestJs/nest-002-mysql/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder().setTitle("BACKEND NEST").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // Acceso a Open API: http://localhost:3000/api
+
+
   await app.listen(3000);
 }
 bootstrap();
