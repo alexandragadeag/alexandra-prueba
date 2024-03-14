8426eacda952579eaa348f4bf6bb7ea311f00bc2
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-14 12:44:34 +0100

    Fullstack Angular book-list.component.ts

--- fullstack/fullstack-001/frontend/src/app/book-list/book-list.component.ts
+++ fullstack/fullstack-001/frontend/src/app/book-list/book-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { Book } from '../interfaces/book.model';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink, NgbAlertModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+
+  books: Book[] = [];
+  showDeletedMessage: boolean = false;
+
+  constructor(private httpClient: HttpClient){}
+  
+  ngOnInit(): void {
+    
+  }
+  delete(){
+
+  }
 
 }
