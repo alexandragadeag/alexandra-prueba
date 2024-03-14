fe2efd83c08041042ca6f8d28c875797e162af37
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-14 11:00:47 +0100

    fullstack angular crear interfaces

---
+++ fullstack/fullstack-001/frontend/src/app/interfaces/author.model.ts
@@ -0,0 +1,7 @@
+export interface Author { 
+    id: number;
+    firstName: string;
+    lastName: string;
+    birhtDate: Date;
+    salary: number;
+}
---
+++ fullstack/fullstack-001/frontend/src/app/interfaces/category.model.ts
@@ -0,0 +1,8 @@
+export interface Category{
+
+    id: number;
+    title: string;
+    description: string;
+    photo: string;
+
+}
