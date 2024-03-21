af0c3533d354d8edb5c1d89d4074828163b7f039
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-21 10:34:03 +0100

    FullStack Author-list con cards

--- fullstack/fullstack-001/frontend/src/app/author-list/author-list.component.ts
+++ fullstack/fullstack-001/frontend/src/app/author-list/author-list.component.ts
@@ -2,11 +2,12 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Author } from '../interfaces/author.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-author-list',
   standalone: true,
-  imports: [HttpClientModule, RouterLink],
+  imports: [HttpClientModule, RouterLink, DatePipe],
   templateUrl: './author-list.component.html',
   styleUrl: './author-list.component.css'
 })
