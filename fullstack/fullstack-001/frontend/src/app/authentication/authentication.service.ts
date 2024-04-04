83adf4bb7bf17062eb11d96f6c65aea317c18311
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-04-04 11:00:14 +0200

    FullStack frontend isAdmin para comprobar rol de usuario AuthService

--- fullstack/fullstack-001/frontend/src/app/authentication/authentication.service.ts
+++ fullstack/fullstack-001/frontend/src/app/authentication/authentication.service.ts
@@ -13,7 +13,7 @@ export class AuthenticationService {
   // Notifica a quien se haya suscrito a este booleano de que ha ocurrido un login
   isLoggedIn = new BehaviorSubject<boolean>(this.hasToken());
   userEmail = new BehaviorSubject<string>(this.getCurrentEmail());
-
+  isAdmin = new BehaviorSubject<boolean>(this.getIsAdmin());
   constructor() { }
 
   hasToken(): boolean {
@@ -25,7 +25,7 @@ export class AuthenticationService {
     localStorage.setItem("jwt_token", token);
     this.isLoggedIn.next(true);
     this.userEmail.next(this.getCurrentEmail());
-  
+    this.isAdmin.next(this.getIsAdmin());
 
 
   }
@@ -37,11 +37,20 @@ export class AuthenticationService {
     const decodedToken = jwtDecode(token) as DecodedToken;
     return decodedToken.email;
     }
+    getIsAdmin(){
+      const token = localStorage.getItem("jwt_token");
+      if(!token)  return false;
+
+      const decodedToken = jwtDecode(token)  as DecodedToken;
+      return decodedToken.role === 'admin';
+    }
+
 
   logout() {
     localStorage.removeItem("jwt_token");
     this.isLoggedIn.next(false);
     this.userEmail.next('');
+    this.isAdmin.next(false);
 
   }
 }
