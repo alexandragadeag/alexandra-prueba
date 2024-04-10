f36eed3ad57128dcab3aecdfa777ca544661cc9f
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-04-10 14:03:22 +0200

    FullStack mostrar errores de autenticación en formulario de login frontend

--- fullstack/fullstack-001/frontend/src/app/login/login.component.ts
+++ fullstack/fullstack-001/frontend/src/app/login/login.component.ts
@@ -19,23 +19,21 @@ export class LoginComponent {
     email: ['', Validators.required],
     password: ['', Validators.required]
   });
+  error = '';
 
   constructor(
     private fb: FormBuilder,
     private httpClient: HttpClient,
     private router: Router,
-    private authService: AuthenticationService) { }
+    private authService: AuthenticationService) {}
 
   save() {
-
     let login: Login = {
       email: this.loginForm.get('email')?.value ?? '',
       password: this.loginForm.get('password')?.value ?? ''
     }
 
-    // Enviar login por POST a backend con http
     let url = 'http://localhost:3000/user/login';
-
     this.httpClient.post<Token>(url, login).subscribe({
       next: data => {
         console.log(data.token);
@@ -44,15 +42,15 @@ export class LoginComponent {
       },
       error: error => {
         console.log(error);
-        if(error.status --- 404) {
-            console.log("No se ha encontrado el usuario");
-        }else if (error.status --- 401) {
-            console.log("Credenciales incorrectas.");
+
+        if (error.status === 404) {
+          this.error = "No se ha encontrado el usuario";
+
+        } else if (error.status === 401) {
+          this.error = "Credenciales incorrectas.";
         }
+
       }
     });
-
-
-
   }
 }
