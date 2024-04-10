6bd0a0618b13696454dc12c69b482c95c0916eed
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-04-10 10:20:48 +0200

    FullStack backend controlador reservas con filtro por usuario en base a rol

--- fullstack/fullstack-001/backend/src/reservation/reservation.controller.ts
+++ fullstack/fullstack-001/backend/src/reservation/reservation.controller.ts
@@ -1,78 +1,95 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, Request } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Reservation } from './reservation.model';
 import { Repository } from 'typeorm';
+import { log } from 'console';
+import { Role } from 'src/user/role.enum';
 
 @Controller('reservation')
 export class ReservationController {
+
 
     constructor(
         @InjectRepository(Reservation) 
         private reservationRepo: Repository<Reservation>
     ) {}
 
-// findAll
-@Get()
-findAll() {
-    return this.reservationRepo.find();
-}
+    @Get()
+    findAll() {
+        return this.reservationRepo.find();
+    }
 
-// findById
-@Get('filter-by-id/:id')
-findById(@Param('id', ParseIntPipe)id: number) {
-    return this.reservationRepo.findOne({
-      where: {
-        id: id
+    @Get('filter-by-id/:id') // :id es una variable, parámetro en la url
+    findById( @Param('id', ParseIntPipe) id: number ) {
+        return this.reservationRepo.findOne({
+            where: {
+                id: id
+            }
+        });
+    }
+    // http://localhost:3000/reservation/filter-by-user/2
+    @Get('filter-by-user/:id')
+    findByUserId(@Param('id', ParseIntPipe) id: number){
+        return this.reservationRepo.find({
+            where: {
+                user: {
+                    id: id
+                }
+            }
+        });
+    }
+
+    // Este método es más seguro para obtener las reservas del usuario autenticado
+    @Get('filter-by-current-user')
+    findByCurrentUserId(@Request() request) {
+
+        if (request.user.role === Role.ADMIN) {
+            return this.reservationRepo.find();
+        } else {
+            return this.reservationRepo.find({
+                where: {
+                    user: {
+                        id: request.user.id
+                    }
+                }
+            });
         }
-    });
-}
 
-@Get('filter-by-user/:id')
-findByUserId(@Param('id', ParseIntPipe) id: number){
-    return this.reservationRepo.findOne({
-        where: {
-            user: {
-                id: id
-              }
-          }
-      });
-}
-
-@Get('filter-by-book/:id')
-findByBookId(@Param('id', ParseIntPipe) id: number){
-    return this.reservationRepo.findOne({
-        where: {
-            book: {
-                id: id
-              }
-          }
-      });
-}
-
-// Filtro dinámico para soportar múltiples parámetros al filtrar
-@Get('filter')
-findWithFilter(@Query() filters: any) {
-      console.log(filters);
-
-      // si está vacio devolver .find() sin filtro
-      if(Object.keys(filters).length === 0)
-           return this.reservationRepo.find();
-
-    // si no está vacio entonces filtrar:
-    return this.reservationRepo.find({
-        where: filters
-    });
-
-}
-
-@Post()
-create(@Body() reservation: Reservation)  {
-    return this.reservationRepo.save(reservation);
-}
+    }   
 
 
+    @Get('filter-by-book/:id')
+    findByBookId(@Param('id', ParseIntPipe) id: number){
+        return this.reservationRepo.find({
+            where: {
+                book: {
+                    id: id
+                }
+            }
+        });
+    }
 
+    // Filtro dinámico para soportar múltiples parámetros al filtrar
+    @Get('filter')
+    findWithFilter(@Query() filters: any) {
+        console.log(filters);
 
+        // Si está vacío devolver .find() sin filtro (No hace falta)
+        // Ejemplo: http://localhost:3000/reservation/filter
+        // if(Object.keys(filters).length === 0) 
+        //    return this.reservationRepo.find();
+
+        // Si no está vacío entonces filtar:
+        // Ejemplo: http://localhost:3000/reservation/filter?user.id=3&startDate=2024-01-01&price=244
+        return this.reservationRepo.find({
+            where: filters
+        });
+    }
+
+    @Post()
+    create(@Body() reservation: Reservation) {
+        return this.reservationRepo.save(reservation);
+    }
 
 
 }
